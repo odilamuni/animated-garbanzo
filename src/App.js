529d60082228b7bd4908127df2763124508e0e8d
Romina Muni <odilamuni@gmail.com>
2018-07-25 13:43:44 +0000

    Font added and linked components to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
+import Stopwatch from './Stopwatch.js';
 import './App.css';
-import Clock from './Clock.js';
-
-
-let removeClock = false;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {!removeClock && <Clock />}
-
-        { !removeClock &&
-          <button type="button" onClick={() => {removeClock = true; this.forceUpdate()}}>Remove clock</button>
-        }
-        { removeClock &&
-          <button type="button" onClick={() => {removeClock = false; this.forceUpdate()}}>Add clock</button>
-        }
+        <Stopwatch/>
       </div>
     );
   }
